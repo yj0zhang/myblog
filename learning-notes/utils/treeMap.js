9ee96e510c7bd589ac30e3de01bda0bad2a458db
yjzhang <yj0zhang@126.com>
2019-07-15 14:58:24 +0800

    Update treeMap.js

--- learning-notes/utils/treeMap.js
+++ learning-notes/utils/treeMap.js
@@ -9,13 +9,13 @@ import map from 'lodash/map';
  * @param {String} parentKey 父索引
  * @param {Any} parentVal 根节点的父
  */
-export function generateTree({data, key, parentKey, parentVal = 0}) {
+export function generateTree({data, key, parentKey, parentVal = 0, childrenKey = "children"}) {
   let tree = [];
   let flattenChild = filter(data, d => d[key] !== parentKey);
   data.forEach((d, i) => {
     if (d[parentKey] === parentVal) {
       tree[i] = d;
-      tree[i].children = generateTree({data: flattenChild, key, parentKey, parentVal: d[key]})
+      tree[i][childrenKey] = generateTree({data: flattenChild, key, parentKey, parentVal: d[key]})
     }
   })
 
@@ -27,12 +27,12 @@ export function generateTree({data, key, parentKey, parentVal = 0}) {
  * 获取树中的所有节点
  * @param {Array} tree 原始数据，树形
  */
-export function getAllNode(tree) {
+export function getAllNode({tree, childrenKey = "children"}) {
   let nodeList = [];
   tree.forEach(d => {
     nodeList.push(d);
-    if (d.children && d.children.length > 0) {
-      nodeList = nodeList.concat(getAllNode(d.children));
+    if (d[childrenKey] && d[childrenKey].length > 0) {
+      nodeList = nodeList.concat(getAllNode(d[childrenKey]));
     }
   });
 
@@ -46,12 +46,12 @@ export function getAllNode(tree) {
  * @param {String} parentKey 父索引
  * @param {Any} parentVal 根节点的父
  */
-export function getChildrenKey({tree, key, parentKey, parentsVals}) {
+export function getChildrenKey({tree, key, parentKey, parentsVals, childrenKey = "children"}) {
   let nodeList = getAllNode(tree), val = [];
   nodeList.forEach(node => {
     if (parentsVals.includes(node[key])) {
-      if (node.children && node.children.length) {
-        val = val.concat(map(node.children, key))
+      if (node[childrenKey] && node[childrenKey].length) {
+        val = val.concat(map(node[childrenKey], key))
       } else {
         val.push(node[key])
       }
@@ -64,7 +64,13 @@ export function getChildrenKey({tree, key, parentKey, parentsVals}) {
 /**
  * 获取所有子节点
  */
+export function getLeafNodes({tree, childrenKey = "children"}) {
+  //todo
+}
 
 /**
  * 获取指定层级的所有节点，从0开始
  */
+export function getNodesByLevel({tree, level, childrenKey = "children"}) {
+  //todo
+}
