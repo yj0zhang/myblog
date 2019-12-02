2d7006c3675fd1437fd24067cacd5bbf70018507
yjzhang <yj0zhang@126.com>
2019-12-02 09:32:19 +0800

    Update camelCase.js

--- learning-notes/utils/camelCase.js
+++ learning-notes/utils/camelCase.js
@@ -15,3 +15,14 @@ function camelCase(obj) {
   }
   return obj;
 }
+
+// 下划线转换驼峰
+function toHump(name) {
+    return name.replace(/\_(\w)/g, function(all, letter){
+        return letter.toUpperCase();
+    });
+}
+// 驼峰转换下划线
+function toLine(name) {
+  return name.replace(/([A-Z])/g,"_$1").toLowerCase();
+}
